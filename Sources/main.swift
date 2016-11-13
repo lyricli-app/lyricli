026a2f69776d0a8c9e15e560bef4a8a01f510aa0
Ben Beltran <ben@nsovocal.com>
2016-11-12 23:23:46 -0600

    Add basic option parsing

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,40 @@
-print("Hello, world!")
+import CommandLineKit
+import Foundation
+
+/// Sets up and returns a new options parser
+/// 
+/// - Returns: A new OptionParser instance
+func createParser() -> ([String:Option], CommandLineKit) {
+
+    let parser = CommandLineKit()
+    var flags: [String:Option] = [:]
+
+    flags["help"] = BoolOption(shortFlag: "h", longFlag: "help", helpMessage: "Prints a help message.")
+
+    parser.addOptions(Array(flags.values))
+
+    return (flags, parser)
+}
+
+func main() {
+
+    let (flags, parser) = createParser()
+
+    do {
+        try parser.parse()
+    }
+    catch {
+        parser.printUsage(error)
+        exit(EX_USAGE)
+    }
+
+    if let helpFlag = flags["help"] as? BoolOption {
+        if helpFlag.value == true {
+                parser.printUsage()
+                exit(0)
+            }
+    }
+
+}
+
+main()
