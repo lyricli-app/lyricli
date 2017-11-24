a12e86c5a465c3e1d19c12d616c57cb64f82b926
Essau <saumotions@gmail.com>
2017-11-24 11:32:08 -0600

    Added Spotify source support. Fixes #1

---
+++ Sources/spotify_source.swift
@@ -0,0 +1,41 @@
+import ScriptingBridge
+
+// Protocol to obtain the track from Spotify
+@objc protocol SpotifyTrack {
+    @objc optional var name: String {get}
+    @objc optional var artist: String {get}
+}
+
+// Protocol to interact with Spotify
+@objc protocol SpotifyApplication {
+    @objc optional var currentTrack: SpotifyTrack? {get}
+}
+
+extension SBApplication : SpotifyApplication {}
+
+// Source that reads track artist and name from current Spotify track
+class SpotifySource: Source {
+
+    // Calls the spotify API and returns the current track
+    var currentTrack: Track? {
+
+        if let spotify: SpotifyApplication = SBApplication(bundleIdentifier: "com.spotify.client") {
+
+            // Attempt to fetch the title from a song
+            if let currentTrack = spotify.currentTrack {
+                if let track = currentTrack {
+                    if let name = track.name {
+                        if let artist = track.artist {
+
+                            return Track(withName: name, andArtist: artist)
+                        }
+                    }
+                }
+            }
+        }
+
+        return nil
+    }
+
+}
+
